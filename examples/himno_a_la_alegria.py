56f2f034231e964b0360bd0b49b4b74dcf7b100f
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-10-29 20:22:57 -0300

    Examples: Trying dynamics.

--- examples/himno_a_la_alegria.py
+++ examples/himno_a_la_alegria.py
@@ -1,14 +1,14 @@
 from musica.conversor import Conversor
 
-chords = "C21/p G C G "
+chords = "C21/mp G C G "
 chords += "C G C G23 C28 C22 "
 chords += "G C G C G a D G "
-chords += "C21 G C G23 C28 C22"
+chords += "C21/mf G21/mp C21/p G23/pp C28 C22"
 
 himno = "E32/pp F34 G | G/p F E D | C/mp C D E | E33/mf D38 D32 "
 himno += "E32/f F34 G | G/ff F E D | C C D E | D33 C38 C32 "
-himno += "D32/p E34 C | D/ff E38 F E34 C | D E38 F E34 D | C D G22 "
-himno += "E32/mp F34 G | G F E D | C C D E | D33 C38 C32"
+himno += "D32/p E34 C | D/ff E38 F E34 C | D34/p E38 F E34 D | C34/ff D G22 "
+himno += "E32/mf F34 G | G34/mp F E D | C34/p C D E | D33/pp C38 C32"
 
 
 c = Conversor(110)
