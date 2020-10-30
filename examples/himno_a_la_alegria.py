7fc4008d58c95ee2632abba65fb6488d86a9d0db
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-10-29 21:05:50 -0300

    Ode to joy example shows and saves the Lilypond score.

--- examples/himno_a_la_alegria.py
+++ examples/himno_a_la_alegria.py
@@ -5,7 +5,7 @@ chords += "C G C G23 C28 C22 "
 chords += "G C G C G a D G "
 chords += "C21/mf G21/mp C21/p G23/pp C28 C22"
 
-himno = "E32/pp F34 G | G/p F E D | C/mp C D E | E33/mf D38 D32 "
+himno = "E32/pp!square F34 G | G/p F E D | C/mp C D E | E33/mf D38 D32 "
 himno += "E32/f F34 G | G/ff F E D | C C D E | D33 C38 C32 "
 himno += "D32/p E34 C | D/ff E38 F E34 C | D34/p E38 F E34 D | C34/ff D G22 "
 himno += "E32/mf F34 G | G34/mp F E D | C34/p C D E | D33/pp C38 C32"
@@ -14,5 +14,12 @@ himno += "E32/mf F34 G | G34/mp F E D | C34/p C D E | D33/pp C38 C32"
 c = Conversor(110)
 c.from_string(himno)
 c.chord_from_string(chords)
+
+s = c.get_lilypond()
+print(s)
+f = open('t.ly', 'w')
+f.write(s)
+f.close()
+
 pm = c.get_pluckmusic()
 pm.play()
